686f989c8642b83eaf02e1d8d910d0f74b60268c
Gupta <ragini-munnaprasad.gupta@capgemini.com>
2021-09-24 14:50:20 +0530

    Microservice added

---
+++ src/main/java/com/complaintservice/ComplaintserviceApplication.java
@@ -0,0 +1,17 @@
+package com.complaintservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * 
+ * @author Ragini
+ *This annotation contain @Configuration , @EnableAutoConfiguration and @ComponentScan
+ */
+@SpringBootApplication
+public class ComplaintserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComplaintserviceApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/complaintservice/entity/Complaint.java
@@ -0,0 +1,95 @@
+package com.complaintservice.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * @author Ragini
+ *
+ */
+@Entity
+@Table(name="complaint")
+public class Complaint {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long complaintId;
+	/**
+	 * Description given as a Complaint it cannot be blank
+	 */
+	@Column(name="descriptions")
+	private String complainDescription;
+	
+	/**
+	 * 
+	 * createdBy field will contain name of farmer retrieved through session
+	 */
+	
+	@Column(name="creators")
+	private String createdBy;
+
+	/**
+	 * Default constructor
+	 */
+	public Complaint() {
+		super();
+	}
+
+	public Complaint(Long complaintId, String complainDescription, String createdBy) {
+		super();
+		this.complaintId = complaintId;
+		this.complainDescription = complainDescription;
+		this.createdBy = createdBy;
+	}
+
+	/**
+	 * @return the complaintId
+	 */
+	public Long getComplaintId() {
+		return complaintId;
+	}
+
+	/**
+	 * @param complaintId the complaintId to set
+	 */
+	public void setComplaintId(Long complaintId) {
+		this.complaintId = complaintId;
+	}
+
+
+	/**
+	 * @return the complainDescription
+	 */
+	public String getComplainDescription() {
+		return complainDescription;
+	}
+
+
+	/**
+	 * @param complainDescription the complainDescription to set
+	 */
+	public void setComplainDescription(String complainDescription) {
+		this.complainDescription = complainDescription;
+	}
+
+
+	/**
+	 * @return the createdBy
+	 */
+	public String getCreatedBy() {
+		return createdBy;
+	}
+
+
+	/**
+	 * @param createdBy the createdBy to set
+	 */
+	public void setCreatedBy(String createdBy) {
+		this.createdBy = createdBy;
+	}
+	
+}
